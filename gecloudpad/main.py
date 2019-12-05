123cb57901c5ef25afd04c71a97bcffe6a3b25cf
makefu <github@syntax-fehler.de>
2019-12-05 21:15:15 +0100

    main.py: add prefix

--- gecloudpad/main.py
+++ gecloudpad/main.py
@@ -12,7 +12,7 @@ def hello():
     response = requests.get('http://blog.binaergewitter.de//podcast_feed/talk/opus/rss.xml?feed_size=1')
     tree = ElementTree.fromstring(response.content)
     prev = int(tree[0][-1][0].text.split('#')[1].split(':')[0])
-    return redirect(f"{app.config['BASEURL']}/p/{prev+1}")
+    return redirect(f"{app.config['BASEURL']}/p/bgt{prev+1}")
 
 if __name__ == '__main__':
     app.run()
